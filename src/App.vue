dbc06e6521483d52060b7b710adf7ccb2b8ad0ef
Mo Jiwa <mohammed.jiwa@gmail.com>
2021-06-08 09:03:50 +0100

    Add styling and data property

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,38 @@
 <template>
-  
+  <div :class="theme === 'light' ? 'body-light' : 'body-dark'" class="main-body">
+    <div class="flex">
+      <div>Theme selection in Vue 3</div>
+      <div></div>
+    </div>
+  </div>
 </template>
 
 <script>
 
 export default {
   name: 'App',
-  components: {
-    
+  data() {
+    return {
+      theme: "dark"
+    }
   }
 }
 </script>
+
+<style>
+.main-body {
+  height: 100vh;
+  padding: 0 2px 0 2px;
+}
+.body-light {
+  background-color: white;
+  color: black;
+}
+.body-dark {
+  background-color: black;
+  color: white;
+}
+.flex {
+  display: flex;
+}
+</style>
